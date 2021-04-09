764f1558db4212dc871463219dfc59a938cd4aff
vinnyyo <60477624+vinnyyo@users.noreply.github.com>
2021-04-09 10:51:06 -0500

    Update GUIDisplay.java

--- src/display/GUIDisplay.java
+++ src/display/GUIDisplay.java
@@ -11,54 +11,83 @@ public class GUIDisplay extends Application implements SystemDisplay {
 
 	}
 
+	/**
+	 * Displays the time until action activates
+	 * 
+	 * @param time - seconds remaining before action activates
+	 */
 	@Override
 	public void showCountDown(int time) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that zones are ready
+	 */
 	@Override
 	public void showZonesReady() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that zones are not ready
+	 */
 	@Override
 	public void showZonesNotReady() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that away is armed
+	 */
 	@Override
 	public void showAwayArmed() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that stay is armed
+	 */
 	@Override
 	public void showStayArmed() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that security is breached
+	 */
 	@Override
 	public void showSecurityBreach() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that away is counting down
+	 */
 	@Override
 	public void showAwayCountDown() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that stay is counting down
+	 */
 	@Override
 	public void showStayCountDown() {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Signify that security breach is counting down
+	 */
 	@Override
 	public void showSecurityBreachCountDown() {
 		// TODO Auto-generated method stub
