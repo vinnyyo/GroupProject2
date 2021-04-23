616343a50c3aa03add700d58caa512283bcc3fa2
vinnyyo <60477624+vinnyyo@users.noreply.github.com>
2021-04-23 16:06:27 -0500

    cancel culture

--- src/states/CancelButtonState.java
+++ src/states/CancelButtonState.java
@@ -1,9 +1,12 @@
 package states;
 
 import events.CorrectCodeEvent;
+import events.MotionDetectedEvent;
+import events.ZonesNotReadyEvent;
 
 /**
  * SystemState to represent while the system is in CancelButtonState
+ * 
  * @author Michael Olson
  *
  */
@@ -20,6 +23,7 @@ public class CancelButtonState extends SystemState {
 
 	/**
 	 * Used to get an instance of the Singleton
+	 * 
 	 * @return Instance of the singleton
 	 */
 	public static CancelButtonState instance() {
@@ -30,26 +34,41 @@ public class CancelButtonState extends SystemState {
 	}
 
 	/**
-	 * Action to take when a CorrectCodeEvent is passed
-	 * Enter ZonesReadyState
+	 * Action to take when a CorrectCodeEvent is passed Enter ZonesReadyState
 	 */
 	@Override
 	public void handleEvent(CorrectCodeEvent event) {
 		if (SystemContext.instance().getZonesReady()) {
 			SystemContext.instance().changeState(ZonesReadyState.instance());
-		} else { 
+		} else {
 			SystemContext.instance().changeState(ZonesNotReadyState.instance());
 		}
 	}
 
 	/**
-	 * Action to take when entering the state
-	 * Display Enter Password
+	 * Action to take when a ZonesNotReadyEvent is passed Change to
+	 * SecurityBreachState
+	 */
+	public void handleEvent(ZonesNotReadyEvent event) {
+		SystemContext.instance().changeState(SecurityBreachState.instance());
+	}
+
+	/**
+	 * Action to take when we get a MotionDetectedEvent, go into
+	 * BreachCountdownState
+	 */
+	@Override
+	public void handleEvent(MotionDetectedEvent event) {
+		SystemContext.instance().changeState(BreachCountdownState.instance());
+	}
+
+	/**
+	 * Action to take when entering the state Display Enter Password
 	 */
 	@Override
 	public void enter() {
 		SystemContext.instance().showEnterPassword();
-		
+
 	}
 
 	/**
@@ -58,7 +77,7 @@ public class CancelButtonState extends SystemState {
 	@Override
 	public void leave() {
 		// Nothing
-		
+
 	}
 
 }
